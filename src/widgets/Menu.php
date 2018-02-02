6da2fabbeedbf23b79f14975416d862cf65b0347
Sergey Morozov <sergmoro1@ya.ru>
2018-02-02 15:14:35 +0300

    add Menu to widgets

--- src/widgets/Menu.php
+++ src/widgets/Menu.php
@@ -1,20 +1,31 @@
 <?php
 namespace sergmoro1\blog\widgets;
 
-use Yii;
 use yii\base\Widget;
 use yii\helpers\Html;
 
 class Menu extends Widget
 {
-    public $items;
-    public $view = 'menu';
+	public $items;
+	public $view = 'menu';
+	public $ulClass = null;
 
+	private function getUrl() {
+		$cid = \Yii::$app->controller->id;
+		$aid = \Yii::$app->controller->action->id;
+		$id = isset($_GET['slug']) ? $_GET['slug'] : (isset($_GET['id']) ? $_GET['id'] : '');
+		$rubric = isset($_GET['rubric']) ? $_GET['rubric'] : '';
+		$tag = isset($_GET['tag']) ? $_GET['tag'] : '';
+		return $cid . '/' . ($aid == 'index' ? ($rubric ? 'rubric/' . $rubric . ($tag ? '/tag/' . $tag : ''): 'index') : ($aid == 'view' ? $id : $aid));
+	}
+    
     public function run()
     {
-        echo $this->render($this->view, [
-            'items' => $this->items,
-        ]);
-    }
+		echo $this->render($this->view, [
+			'items' => $this->items,
+			'url' => $this->getUrl(),
+			'ulClass' => $this->ulClass,
+		]);
+	}
 }
 
