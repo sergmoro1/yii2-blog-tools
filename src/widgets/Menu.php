a40c418a8d78c7470d66fc04c781c8f395b8f1f8
Sergey Morozov <sergmoro1@ya.ru>
2018-02-15 23:07:37 +0300

    fix highlight current menu item

--- src/widgets/Menu.php
+++ src/widgets/Menu.php
@@ -11,12 +11,22 @@ class Menu extends Widget
 	public $ulClass = null;
 
 	private function getUrl() {
+		$mid = \Yii::$app->controller->module->id == \Yii::$app->id ? '' : \Yii::$app->controller->module->id;
 		$cid = \Yii::$app->controller->id;
 		$aid = \Yii::$app->controller->action->id;
 		$id = isset($_GET['slug']) ? $_GET['slug'] : (isset($_GET['id']) ? $_GET['id'] : '');
 		$rubric = isset($_GET['rubric']) ? $_GET['rubric'] : '';
 		$tag = isset($_GET['tag']) ? $_GET['tag'] : '';
-		return $cid . '/' . ($aid == 'index' ? ($rubric ? 'rubric/' . $rubric . ($tag ? '/tag/' . $tag : ''): 'index') : ($aid == 'view' ? $id : $aid));
+		return ($mid ? $mid. '/' : '') . 
+			$cid . '/' . 
+			($aid == 'index' 
+			    ? ($rubric 
+			        ? 'rubric/' . $rubric . ($tag 
+			            ? '/tag/' . $tag 
+			            : '') 
+			        : 'index') 
+			    : ($aid == 'view' ? $id : $aid)
+			);
 	}
     
     public function run()
--- src/widgets/views/menu.php
+++ src/widgets/views/menu.php
@@ -10,9 +10,9 @@ use sergmoro1\blog\Module;
 
     <?php foreach($items as $name => $item): ?>
 
-        <?php if(isset($item['admin']) && $item['admin'] && !(Yii::$app->user->identity->group == common\models\User::GROUP_ADMIN)) continue; ?>
+        <?php if(isset($item['admin']) && $item['admin'] && !(\Yii::$app->user->identity->group == common\models\User::GROUP_ADMIN)) continue; ?>
 
-        <li <?= Yii::$app->request->pathInfo == $item['url'] ? 'class="active"' : '' ?>>
+        <li <?= $url == $item['url'] ? 'class="active"' : '' ?>>
             <?= Html::a('<i class="fa fa-1fw fa-'. $item['icon'] .'"></i> '. Module::t('core', $item['caption']), ['/' . $item['url']]); ?>
         </li>
     <?php endforeach; ?>
