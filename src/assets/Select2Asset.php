5581f7af0d578aa71d8250e1ecc679cbed2a34d4
Sergey Morozov <sergmoro1@ya.ru>
2018-02-19 13:57:42 +0300

    inherit hiqdev/Select2Asset

--- src/assets/Select2Asset.php
+++ src/assets/Select2Asset.php
@@ -4,9 +4,14 @@ namespace sergmoro1\blog\assets;
 
 class Select2Asset extends \hiqdev\yii2\assets\select2\Select2Asset
 {
-    /**
-     * {@inheritdoc}
-     */
     public $sourcePath = '@vendor/bower-asset/select2/dist';
+
+    public $js = [
+        'js/select2.full.min.js',
+    ];
+
+    public $css = [
+        'css/select2.min.css',
+    ];
 
 }
