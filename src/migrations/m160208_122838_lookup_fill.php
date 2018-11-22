5c638025687e3bd72bda66ffcfb8da5d3a19d556
Sergey Morozov <sergmoro1@ya.ru>
2018-11-22 22:07:27 +0300

    add const to lookupfill

--- src/migrations/m160208_122838_lookup_fill.php
+++ src/migrations/m160208_122838_lookup_fill.php
@@ -5,36 +5,36 @@ use yii\db\Migration;
 
 class m160208_122838_lookup_fill extends Migration
 {
-    const TABLE = '{{%lookup}}';
+    const LOOKUP = '{{%lookup}}';
     const PROPERTY = '{{%property}}';
-    const PID = 3; // property ID
+    // Properties
+    const POST_STATUS = 3;
+    const COMMENT_STATUS = 4;
+    const COMMENT_FOR = 5;
+    
     public function up()
     {
-        $i = self::PID;
-        $this->insert(self::PROPERTY, ['id' =>  $i, 'name' => 'PostStatus']);
-        $this->insert(self::TABLE, ['name' => 'Черновик', 'code' => 1, 'property_id' => $i, 'position' => 1]);
-        $this->insert(self::TABLE, ['name' => 'Опубликовано', 'code' => 2, 'property_id' => $i, 'position' => 2]);
-        $this->insert(self::TABLE, ['name' => 'Архив', 'code' => 3, 'property_id' => $i, 'position' => 3]);
+        $this->insert(self::PROPERTY, ['id' =>  self::POST_STATUS, 'name' => 'PostStatus']);
+        $this->insert(self::LOOKUP, ['name' => 'Черновик', 'code' => 1, 'property_id' => self::POST_STATUS, 'position' => 1]);
+        $this->insert(self::LOOKUP, ['name' => 'Опубликовано', 'code' => 2, 'property_id' => self::POST_STATUS, 'position' => 2]);
+        $this->insert(self::LOOKUP, ['name' => 'Архив', 'code' => 3, 'property_id' => self::POST_STATUS, 'position' => 3]);
 
-        $i++;
         $this->insert(self::PROPERTY, ['id' => $i, 'name' => 'CommentStatus']);
-        $this->insert(self::TABLE, ['name' => 'Ожидание', 'code' => 1, 'property_id' => $i, 'position' => 1]);
-        $this->insert(self::TABLE, ['name' => 'Подтверждено', 'code' => 2, 'property_id' => $i, 'position' => 2]);
-        $this->insert(self::TABLE, ['name' => 'Архив', 'code' => 3, 'property_id' => $i, 'position' => 3]);
+        $this->insert(self::LOOKUP, ['name' => 'Ожидание', 'code' => 1, 'property_id' => self::COMMENT_STATUS, 'position' => 1]);
+        $this->insert(self::LOOKUP, ['name' => 'Подтверждено', 'code' => 2, 'property_id' => self::COMMENT_STATUS, 'position' => 2]);
+        $this->insert(self::LOOKUP, ['name' => 'Архив', 'code' => 3, 'property_id' => self::COMMENT_STATUS, 'position' => 3]);
 
-        $i++;
         $this->insert(self::PROPERTY, ['id' => $i, 'name' => 'CommentFor']);
-        $this->insert(self::TABLE, ['name' => 'Статьи', 'code' => 1, 'property_id' => $i, 'position' => 1]);
+        $this->insert(self::LOOKUP, ['name' => 'Статьи', 'code' => 1, 'property_id' => self::COMMENT_FOR, 'position' => 1]);
     }
 
     public function down()
     {
-        $i = self::PID;
-        $this->delete(self::TABLE, 'property_id=' . $i);
-        $this->delete(self::TABLE, 'property_id=' . ($i + 1));
-        $this->delete(self::TABLE, 'code=1 AND prooerty_id=' . ($i + 2));
-        $this->delete(self::PROPERTY, 'id=' . $i);
-        $this->delete(self::PROPERTY, 'id=' . ($i + 1));
-        $this->delete(self::PROPERTY, 'id=' . ($i + 2));
+        $this->delete(self::LOOKUP, 'property_id=' . self::POST_STATUS);
+        $this->delete(self::LOOKUP, 'property_id=' . self::COMMENT_STATUS);
+        $this->delete(self::LOOKUP, 'code=1 AND prooerty_id=' . self::COMMENT_FOR);
+        $this->delete(self::PROPERTY, 'id=' . self::POST_STATUS);
+        $this->delete(self::PROPERTY, 'id=' . self::COMMENT_STATUS);
+        $this->delete(self::PROPERTY, 'id=' . self::COMMENT_FOR);
     }
 }
