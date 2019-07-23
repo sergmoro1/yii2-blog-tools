ad6baf5c101dc7acb6b7a33d1c06207421c7b958
Sergey Morozov <sergmoro1@ya.ru>
2019-07-23 14:46:25 +0300

    Move assets to assets/src.

--- src/assets/SBAdminAsset.php
+++ src/assets/SBAdminAsset.php
@@ -1,7 +1,6 @@
 <?php
 /**
- * @author <sergmoro1@ya.ru>
- * @license GPL
+ * @author Sergey Morozov <sergmoro1@ya.ru>
  */
 
 namespace sergmoro1\blog\assets;
@@ -10,7 +9,7 @@ use yii\web\AssetBundle;
 
 class SBAdminAsset extends AssetBundle
 {
-    public $sourcePath = '@vendor/sergmoro1/yii2-blog-tools/src/assets';
+    public $sourcePath = '@vendor/sergmoro1/yii2-blog-tools/src/assets/src';
     public $css = [
         'css/sb-admin.css',
         'css/site.css',
